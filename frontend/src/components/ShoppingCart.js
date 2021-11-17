21696b6cb4726d9f152b4afdfb5b255ac0d84053
jardilam <jfardilam@outlook.com>
2021-11-17 09:43:42 -0500

    Finalización de la lógica del carrito de compras 17112021

--- frontend/src/components/ShoppingCart.js
+++ frontend/src/components/ShoppingCart.js
@@ -1,40 +1,88 @@
-import {useReducer} from 'react'
-import { TYPES } from '../actions/Shopping';
-import {shoppingReducer, shoppingInitialState} from '../reducer/shoppingReducer'
+import {
+  useReducer
+} from 'react'
+import {
+  TYPES
+} from '../actions/Shopping';
+import {
+  shoppingReducer,
+  shoppingInitialState
+} from '../reducer/shoppingReducer'
 import CartItem from './CartItem';
 import Productcards from "./Productcards";
 
 const ShoppingCart = () => {
-  const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState);
+    const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState);
 
-  const { products, cart } = state;
+    const {
+      products,
+      cart
+    } = state;
 
-  const addToCart = (id) => {
-    console.log(id);
-    dispatch({type: TYPES.addToCart, payload: id});
-  };
+    const addToCart = (id) => {
+      console.log(id);
+      dispatch({
+        type: TYPES.addToCart,
+        payload: id
+      });
+    };
 
-  const delFromCart = () => {};
+    const delFromCart = (id, all = false) => {
+      console.log(id, all)
+      if (all) {
+        dispatch({
+          type: TYPES.removeAll,
+          payload: id
+        })
+      } else {
+        dispatch({
+          type: TYPES.removeOne,
+          payload: id
+        })
+      }
+    };
 
-  const clearCart = () => {};
+    const clearCart = () => {
+      dispatch({
+        type: TYPES.clearCart
+      })
+    };
 
-  return (
-    <div>
-      <h2>Carrito de Compras</h2>
-      <h3>Productos</h3>
-      <article className="box grid-responsive">
-        {products.map((product) => (<Productcards key={product.id} data={product} addToCart={addToCart} />))}
-      </article>
-      <h3>Carrito</h3>
-      <article className="box">
-        <button onClick={clearCart}>Limpiar carrito</button>
-        {
-          cart.map((item, index) => (<CartItem key={item.id} data={item} delFromCart={delFromCart} />))
-        }
-      </article>
-    </div>
-  );
-};
+    return ( <div>
+        <h2> Carrito de Compras </h2> 
+        <h3> Productos </h3> 
+        <article className = "box grid-responsive"> {
+          products.map((product) => ( <Productcards key = {
+                product.id
+              }
+              data = {
+                product
+              }
+              addToCart = {
+                addToCart
+              }
+              />))} 
+          </article> 
+          <h3> Carrito </h3> 
+          <article className = "box">
+              <button onClick = {
+                clearCart
+              }> Limpiar carrito </button> {
+                cart.map((item, index) => ( < CartItem key = {
+                      item.id
+                    }
+                    data = {
+                      item
+                    }
+                    delFromCart = {
+                      delFromCart
+                    }
+                    />))
+                  } 
+                  </article> 
+                  </div>
+                );
+              };
 
 
-export default ShoppingCart;
+              export default ShoppingCart;
--- frontend/src/reducer/shoppingReducer.js
+++ frontend/src/reducer/shoppingReducer.js
@@ -1,12 +1,33 @@
-import {TYPES} from '../actions/Shopping'
+import {
+    TYPES
+} from '../actions/Shopping'
 
 export const shoppingInitialState = {
-    products: [
-        {id: 1, name: "Lomo Fino de Res", price: 32000},
-        {id: 2, name: "Churrasco", price: 24000},
-        {id: 3, name: "Punta de Anca", price: 24000},
-        {id: 4, name: "Centro de Cadera", price: 18800},
-        {id: 5, name: "T-Bone", price: 32000},
+    products: [{
+            id: 1,
+            name: "Lomo Fino de Res",
+            price: 32000
+        },
+        {
+            id: 2,
+            name: "Churrasco",
+            price: 24000
+        },
+        {
+            id: 3,
+            name: "Punta de Anca",
+            price: 24000
+        },
+        {
+            id: 4,
+            name: "Centro de Cadera",
+            price: 18800
+        },
+        {
+            id: 5,
+            name: "T-Bone",
+            price: 32000
+        },
     ],
     cart: []
 };
@@ -15,20 +36,42 @@ export function shoppingReducer(state, action) {
     switch (action.type) {
         case TYPES.addToCart: {
             let newItem = state.products.find(product => product.id === action.payload);
-            return {
+            let itemInCart = state.cart.find(item => item.id === newItem.id)
+            return itemInCart ? {
                 ...state,
-                cart: [...state.cart, newItem]
-            }
+                cart: state.cart.map(item => item.id === newItem.id ? {
+                    ...item,
+                    quantity: item.quantity + 1
+                } : item)
+            } : {
+                ...state,
+                cart: [...state.cart, {
+                    ...newItem,
+                    quantity: 1
+                }]
+            };
         }
         case TYPES.removeOne: {
-
+            let itemToDelete = state.cart.find(item => item.id === action.payload);
+            return itemToDelete.quantity > 1 ? {
+                ...state,
+                cart: state.cart.map(item => item.id === action.payload ? {
+                    ...item,
+                    quantity: item.quantity - 1
+                } : item)
+            } : {
+                ...state,
+                cart: state.cart.filter(item => item.id !== action.payload)
+            };
         }
         case TYPES.removeAll: {
-
+            return {
+                ...state,
+                cart: state.cart.filter((item) => item.id !== action.payload)
+            };
         }
-        case TYPES.clearCart: {
-
-        }
+        case TYPES.clearCart:
+            return shoppingInitialState;
         default:
             return state;
     }
